c703da94586d6c11546d0fea4d79bc0d428b08d1
dochoixe <trongluu13thg05@gmail.com>
2026-08-12 10:55:50 +0700

    Update config-links.js

--- config-links.js
+++ config-links.js
@@ -5,7 +5,8 @@
 window.DOCHOIXE99_CONFIG = {
   HOME_URL: "https://dochoixe.github.io/",
   BRAKE_PAGE_PATH: "he-thong-phanh.html",
-
+  BRAKE_PAGE_PATH: "cnc-he-thong-treo.html",
+ 
   PHONE_DISPLAY: "0878 976 186",
   PHONE_RAW: "0878976186",
 
