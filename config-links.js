617f5f450a34b62229f6d331857935af856f70a6
dochoixe <trongluu13thg05@gmail.com>
2026-08-17 12:12:51 +0700

    Add files via upload

--- config-links.js
+++ config-links.js
@@ -10,5 +10,8 @@ window.DOCHOIXE99_CONFIG = {
   PHONE_DISPLAY: "0878 976 186",
   PHONE_RAW: "0878976186",
 
-  WEBSITE_LABEL: "dochoixe.github.io"
+  WEBSITE_LABEL: "dochoixe.github.io",
+
+  /* Dán URL Google Apps Script kết thúc bằng /exec vào đây */
+  LEAD_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwSodVPdTY1mkf1u-7IeXBlLAap0nFBlc50dyZ1kKx6-1RjV3_OsXaVpFgZ9QABnDSj/exec"
 };
